032e7b96c86c6302a021c437a255f3fc70fbf155
RealMrSmartypants <info@audaxgroup.pro>
2026-05-14 17:07:00 -0400

    Serve static files from the public directory
    Add static file serving for the SDK from the public directory.

--- routes/server.js
+++ routes/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/sdk', express.static(path.join(__dirname, '..', 'public')));
 
 const twilioRoutes = require('./twilio');
 app.use('/api', twilioRoutes);
